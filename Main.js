e64b66ca8303a762ea4a39db1939c8ee121b29a7
Shadow14032001 <kmikeeddy@gmail.com>
2023-07-07 15:33:20 +0100

    new change

--- Main.js
+++ Main.js
@@ -1,6 +1,7 @@
 console.log("Bonjour le monde!");
-// function helloWorld() {
-//   return 'Hello World!';
-// }
+function helloWorld() {
+  return 'Hello World!';
+}
 
-// module.exports = helloWorld;
+module.exports = helloWorld;
+console.log(helloWorld());
